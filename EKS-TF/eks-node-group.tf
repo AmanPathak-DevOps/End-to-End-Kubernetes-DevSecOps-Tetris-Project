5329c2cd638992715049887aabd3e39c54a8badd
christyl23 <120269677+christyl23@users.noreply.github.com>
2024-03-15 18:59:13 -0400

    updates

--- EKS-TF/eks-node-group.tf
+++ EKS-TF/eks-node-group.tf
@@ -2,7 +2,7 @@ resource "aws_eks_node_group" "eks-node-group" {
   cluster_name    = aws_eks_cluster.eks-cluster.name
   node_group_name = var.eksnode-group-name
   node_role_arn   = aws_iam_role.NodeGroupRole.arn
-  subnet_ids      = [data.aws_subnet.subnet.id, aws_subnet.public-subnet2.id]
+  subnet_ids      = [data.aws_subnet.dove-pub-subnet.id, aws_subnet.dove-pub-subnet2.id]
 
 
   scaling_config {
