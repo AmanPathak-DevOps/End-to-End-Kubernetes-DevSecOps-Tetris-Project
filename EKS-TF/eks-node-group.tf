6f8c8c8e358f7e7b359e06d8d6a9e0f4dd9d7c08
PatelUtkarsh-tech <utkarshpatel73008@gmail.com>
2025-02-27 16:50:54 +0530

    Update eks-node-group.tf

--- EKS-TF/eks-node-group.tf
+++ EKS-TF/eks-node-group.tf
@@ -12,7 +12,7 @@ resource "aws_eks_node_group" "eks-node-group" {
   }
 
   ami_type       = "AL2_x86_64"
-  instance_types = ["t2.micro"]
+  instance_types = ["t2.medium"]
   disk_size      = 20
 
   depends_on = [
