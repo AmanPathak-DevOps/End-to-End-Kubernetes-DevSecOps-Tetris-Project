ba39f6dbc353ad148149be56c0d26e523a38fd6e
PatelUtkarsh-tech <utkarshpatel73008@gmail.com>
2025-03-02 00:39:30 +0530

    Update ec2.tf

--- Jenkins-Server-TF/ec2.tf
+++ Jenkins-Server-TF/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ec2" {
   ami                    = data.aws_ami.ami.image_id
   instance_type          = "t2.2xlarge"
-  key_name               = var.key-name
+  #key_name               = var.key-name
   subnet_id              = aws_subnet.public-subnet.id
   vpc_security_group_ids = [aws_security_group.security-group.id]
   iam_instance_profile   = aws_iam_instance_profile.instance-profile.name
