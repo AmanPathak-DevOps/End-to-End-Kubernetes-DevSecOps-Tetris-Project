be9ac73bba9052de4d7b4b8c31936ea8f33f3988
AmanPathak-DevOps <aman07pathak@gmail.com>
2025-11-29 15:13:31 +0530

    fix: backend for eks-tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,12 +1,12 @@
 terraform {
   backend "s3" {
-    bucket         = "dev-aman-tf-bucket"
-    region         = "us-east-1"
-    key            = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/EKS-TF/terraform.tfstate"
+    bucket       = "dev-aman-tf-bucket"
+    region       = "us-east-1"
+    key          = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/EKS-TF/terraform.tfstate"
     use_lockfile = true
-    encrypt        = true
+    encrypt      = true
   }
-  required_version = ">=1.13.3"
+  required_version = ">=1.14.0"
   required_providers {
     aws = {
       version = ">= 5.49.0"
