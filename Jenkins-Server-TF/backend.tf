0fb8943e52a7c465637ce29e501ecec480f27682
AmanPathak-DevOps <aman07pathak@gmail.com>
2025-11-30 10:03:47 +0530

    fix: final version

--- Jenkins-Server-TF/backend.tf
+++ Jenkins-Server-TF/backend.tf
@@ -1,15 +1,15 @@
 terraform {
   backend "s3" {
-    bucket         = "dev-aman-tf-bucket"
-    region         = "us-east-1"
-    key            = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/Jenkins-Server-TF/terraform.tfstate"
-    encrypt        = true
+    bucket       = "dev-aman-tf-bucket"
+    region       = "us-east-1"
+    key          = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/Jenkins-Server-TF/terraform.tfstate"
+    encrypt      = true
     use_lockfile = true
   }
   required_version = ">=1.13.3"
   required_providers {
     aws = {
-      version = ">= 5.49.0"
+      version = ">= 6.23.0"
       source  = "hashicorp/aws"
     }
   }
---
+++ Process.md
@@ -0,0 +1,11 @@
+# Process.md
+
+1. Create Jenkins/EC2 machines via TF: Completed
+2. Configure Jenkins for AWS integration: Completed
+3. Create the Job and deploy EKS Cluster using Jenkins: Completed
+4. Configure Jenkins for multiple tools including Sonarqube: Completed
+5. Deploy Source Code via Jenkins on EKS
+
+
+
+squ_4c3cf80536aa39145b4e69b5c6149135ef45ece4
